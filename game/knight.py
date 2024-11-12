c050c9eb8612da8c09056371ec22af412211887f
IkramHeraoua <heraoua.ikramm@gmail.com>
2024-11-12 23:13:20 +0100

    commit3

---
+++ game/knight.py
@@ -0,0 +1,36 @@
+
+class knight:
+    def __init__(self, chromosome=None):
+        self.position = [0, 0]
+        self.chromosome = chromosome if chromosome is not None else chromosome.chromosome()
+        self.path = [self.position]
+        self.fitness = 0
+
+        self.moves = {
+            1: [1, -2],  # up-right
+            2: [2, -1],  # right-up
+            3: [2, 1],  # right-down
+            4: [1, 2],  # down-right
+            5: [-1, 2],  # down-left
+            6: [-2, 1],  # left-down
+            7: [-2, -1],  # left-up
+            8: [-1, -2]  # up-left
+        }
+
+    def move_forward(self, direction):
+        x, y = self.position
+        if direction in self.moves:
+            dx, dy = self.moves[direction]
+            new_position = (x + dx, y + dy)
+            self.position = new_position
+            self.path.append(new_position)
+            return new_position
+        else:
+            raise ValueError("Invalid direction. Must be an integer between 1 and 8.")
+
+    def move_backward(self):
+        if len(self.path) > 1:
+            self.path.pop()
+            self.position = self.path[-1]
+        else:
+            raise ValueError("Cannot move backward from the initial position.")
---
+++ genetic/chromosome.py
@@ -0,0 +1,45 @@
+import random
+
+class chromosome:
+    def __init__(self, genes=None):
+        if genes is None:
+            # Générer 63 mouvements aléatoires (1-8)
+            self.genes = [random.randint(1, 8) for _ in range(63)]
+        else:
+            self.genes = genes
+
+    def crossover(self , partner):
+        point = random.randint(0, len(self.genes) - 1)
+        child1_genes = self.genes[:point] + partner.genes[point:]
+        child2_genes = partner.genes[:point] + self.genes[point:]
+        return chromosome(child1_genes), chromosome(child2_genes)
+
+    def mutation(self):
+        # Choisir un gène aléatoire à muter
+        index_to_mutate = random.randint(0, len(self.genes) - 1)
+        print(f"Index choisi pour mutation: {index_to_mutate}")
+
+        # Assigner une nouvelle valeur aléatoire au gène, différente de l'actuelle
+        current_value = self.genes[index_to_mutate]
+        new_value = current_value
+        while new_value == current_value:
+            new_value = random.randint(0, 7)
+
+        self.genes[index_to_mutate] = new_value
+        print(f"Valeur avant mutation: {current_value}, Valeur après mutation: {new_value}")
+
+# Création de deux chromosomes aléatoires
+chromosome1 = chromosome()
+chromosome2 = chromosome()
+
+print("Chromosome 1 initial:", chromosome1.genes)
+print("Chromosome 2 initial:", chromosome2.genes)
+
+# Croisement des deux chromosomes
+child1, child2 = chromosome1.crossover(chromosome2)
+print("Enfant 1 après croisement:", child1.genes)
+print("Enfant 2 après croisement:", child2.genes)
+
+# Mutation du premier chromosome
+chromosome1.mutation()
+print("Chromosome 1 après mutation:", chromosome1.genes)
